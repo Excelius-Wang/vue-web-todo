171e1bb0b4f94ae5062f0856921b92d2de54ccfa
WangZhao <2419102388@qq.com>
2024-12-27 14:40:17 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+import ToDoItem from "@/components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
+import { ref } from 'vue';
+
+// 创建 ref 来管理状态
+const ToDoItems = ref([
+    { id: uniqueId("todo-"), label: "Learn Vue", done: false },
+    {
+        id: uniqueId("todo-"),
+        label: "Create a Vue project with the CLI",
+        done: true,
+    },
+    { id: uniqueId("todo-"), label: "Have fun", done: true },
+    { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
+]);
+</script>
+
+<template>
+    <div id="app">
+        <h1>To-Do List</h1>
+        <ul>
+            <li>
+                <to-do-item label="My ToDo Item" :done="true"></to-do-item>
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+<style scoped>
+header {
+    line-height: 1.5;
+}
+
+.logo {
+    display: block;
+    margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .logo {
+        margin: 0 2rem 0 0;
+    }
+
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
+}
+</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,27 @@
+<script setup>
+import uniqueId from "lodash.uniqueid";
+
+const props = defineProps({
+    props: {
+        label: {required: true, type: String},
+        done: {default: false, type: Boolean}
+    },
+    data() {
+        return {
+            isDone: this.done,
+            id: uniqueId("todo-"),
+        };
+    },
+});
+</script>
+
+<template>
+    <div>
+        <input type="checkbox" :id="id" :checked="isDone"/>
+        <label :for="id">{{ label }}</label>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
